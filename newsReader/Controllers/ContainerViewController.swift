cf3c2e636cc398df3bf4ef156d9b6aa7296b415b
Ivan <ivan44364yr@gmail.com>
2022-01-25 12:32:33 +0300

    add folders

---
+++ newsReader/Controllers/ContainerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContainerViewController.swift
+//  newsReader
+//
+//  Created by Ivan Karpovich on 10.01.22.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
